b75d32a74202467d05dbf465f4980fb240787607
Masahji Stewart <masahji@synctree.com>
2025-02-15 12:49:34 -0800

    fix: Improve footer button responsiveness in kiosk mode

--- components/InformationCenterFooter.tsx
+++ components/InformationCenterFooter.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export function InformationCenterFooter() {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 bg-black bg-opacity-20">
-      <Link href="/information-center" className="w-full p-8 flex flex-col items-center gap-4">
+    <footer className="fixed bottom-0 left-0 right-0 bg-black bg-opacity-20 z-50">
+      <Link href="/information-center" className="w-full p-12 flex flex-col items-center gap-6 touch-manipulation">
         <img 
           src="/srf-logo.png" 
           alt="Self-Realization Fellowship Logo" 
