fd1c9ca317cfe44f34b08947ff729a816937b732
Masahji Stewart <masahji@synctree.com>
2025-02-15 12:48:29 -0800

    feat: Enhance InformationCenterFooter with larger icon and full-area click

--- components/InformationCenterFooter.tsx
+++ components/InformationCenterFooter.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link'
 
 export function InformationCenterFooter() {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 p-8 flex flex-col items-center gap-4 bg-black bg-opacity-20">
-      <Link href="/information-center">
+    <footer className="fixed bottom-0 left-0 right-0 bg-black bg-opacity-20">
+      <Link href="/information-center" className="w-full p-8 flex flex-col items-center gap-4">
         <img 
           src="/srf-logo.png" 
           alt="Self-Realization Fellowship Logo" 
-          className="h-16 w-16"
+          className="h-24 w-24"
         />
+        <p className="text-3xl text-white">Touch to return home</p>
       </Link>
-      <p className="text-3xl text-white">Touch to return home</p>
     </footer>
   );
 }
